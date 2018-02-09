60f50df20a99f41f8935335b144fedec22c9c69b
dave108 <32458148+davey108@users.noreply.github.com>
2018-02-08 22:07:58 -0500

    Update QuoteCMD.java

--- QuoteCMD.java
+++ QuoteCMD.java
@@ -2,7 +2,7 @@ package quotes;
 import java.util.Scanner;
 
 /* This class runs the command line interface for the quote server
- * Author: Khang Chau Vo
+ * @author Khang Chau Vo
  * Last Updated: 2/8/2018
  */
 public class QuoteCMD{
