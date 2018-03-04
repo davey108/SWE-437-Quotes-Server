44f73a75073f7eceea690b6e25850a7a09be6b86
dave108 <32458148+davey108@users.noreply.github.com>
2018-03-03 22:22:38 -0500

    Update NoKeyWordException.java

--- NoKeyWordException.java
+++ NoKeyWordException.java
@@ -1,5 +1,8 @@
 
-
+/**
+ * This class is a custom exception used by QuoteCMD.java in the method searchKey()
+ * Will be thrown when a none existence key is inputted into the above mentioned method
+ */
 public class NoKeyWordException extends Exception {
 
 	/**
